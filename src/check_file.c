218d43dd2954a00f28a15e70d183c710ff6df761
atambo <alex.tambo.15432@gmail.com>
2025-04-25 01:30:55 +0100

    basic file checks done

--- src/check_file.c
+++ src/check_file.c
@@ -6,7 +6,7 @@
 /*   By: atambo <atambo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 00:51:59 by atambo            #+#    #+#             */
-/*   Updated: 2025/04/25 01:10:10 by atambo           ###   ########.fr       */
+/*   Updated: 2025/04/25 01:29:18 by atambo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,23 @@
 int ft_check_file(int ac, char **av)
 {
 	int fd;
+	char buffer[1];
 	
 	if (ac == 1)
-	exit (ft_perror("Need a file as input\n", 1));
+	exit (ft_perror("miniRT: Need a file_path as param\n", 1));
 	if (ac > 2)
-	exit (ft_perror("Too many params, only need one file\n", 1));
+	exit (ft_perror("miniRT: Too many params, only need one file_path\n", 1));
     fd = open(av[1], O_RDONLY);
     if (fd == -1)
     {
-		printf("%s\n", strerror(errno));
+		printf("miniRT: '%s': %s\n", av[1], strerror(errno));
         exit (1);
+    }
+	if (read(fd, buffer, 1) == -1 && errno == EISDIR)
+    {
+        printf("miniRT: '%s' is a directory, not a file\n", av[1]);
+        close(fd);
+        exit(1);
     }
 	return (fd);
 }
