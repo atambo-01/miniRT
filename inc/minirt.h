e424e622ffb37db7f8ad2a0ea75cb4db3b815664
atambo <alex.tambo.15432@gmail.com>
2025-04-25 12:22:51 +0100

    Created separate img struct

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,7 +6,7 @@
 /*   By: atambo <atambo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 22:55:42 by atambo            #+#    #+#             */
-/*   Updated: 2025/04/25 01:50:19 by atambo           ###   ########.fr       */
+/*   Updated: 2025/04/25 04:37:17 by atambo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,25 +30,43 @@
 # define RIGHT		65364
 # define W_WIDTH	1080
 # define W_HEIGHT	720
-# define IM_WIDTH	1080
-# define IM_HEIGHT	720
+# define IM_WIDTH	540
+# define IM_HEIGHT	360
 
-typedef struct	s_data
+typedef struct	s_img
 {
-	void*	mlx;
-	void*	win;
-	void*	img;
+	void*	ptr;
 	void*	addr;
 	int		line_len;
 	int		bpp;
 	int		endian;
+}				t_img;
+typedef struct	s_data
+{
+	void*	mlx;
+	void*	win;
 	int		fd;
+	t_img	img;
+	t_img	s_img;
 }				t_data;
 
-int	ft_perror(char const *msg, int e);
-int ft_check_file(int ac, char **av);
-int ft_init_data(t_data *data, int fd);
-int	key_hook(int keycode, t_data *data);
-int	close_window(t_data *data);
+// src/check_file.c
+int 	ft_check_file(int ac, char **av);
+// ft_perror.c
+int		ft_perror(char const *msg, int e);
+// src/init.c
+int 	ft_init_data(t_data *data, int fd);
+// src/input.c
+int		key_hook(int keycode, t_data *data);
+int		mouse_hook(int button, int x, int y, t_data *data);
+// src/input_util.c
+int		close_window(t_data *data);
+// src/upscale_img.c
+void	fill_color(t_data *data, int color);
+void	fill_tiled(t_img *img, t_data *data);
+void	upscale_img(t_data *data);
+// src/util.c
+void	pixel_put_img(t_img *img, int x, int y, int color);
+
 
 #endif
