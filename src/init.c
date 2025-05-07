b6cb5b7ae0e48766e391dee24f2cf583ae1ddfc1
atambo <alex.tambo.15432@gmail.com>
2025-05-07 11:21:10 +0100

    Started to implement  a_light Editing the doc Will not inplement light njust yet

--- src/init.c
+++ src/init.c
@@ -6,23 +6,29 @@
 /*   By: atambo <atambo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 00:13:05 by atambo            #+#    #+#             */
-/*   Updated: 2025/05/07 00:27:01 by atambo           ###   ########.fr       */
+/*   Updated: 2025/05/07 03:04:26 by atambo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minirt.h"
+
+int ft_init_alight(t_data *data)
+{
+	data->alight.color = 0xffffff;
+	data->alight.ratio = 0.5;
+	return (ft_init_obj(data));
+}
 
 int ft_init_cam(t_data *data)
 {
 	ft_setvec3(&data->cam.pos, 0, 0, -15);
 	ft_setvec3(&data->cam.dir, 0, 0, 1);
     data->cam.fov = 60.0;
-	return (ft_init_obj(data));
+	return (ft_init_alight(data));
 }
 
-int ft_init_data(t_data *data, int fd)
+void ft_init_mlx(t_data *data)
 {
-	data->fd = fd;
 	data->mlx = mlx_init();
 	if (!data->mlx)
 	{
@@ -38,13 +44,18 @@ int ft_init_data(t_data *data, int fd)
 	}
 	data->img.ptr = mlx_new_image(data->mlx, IM_WIDTH, IM_HEIGHT);
 	data->s_img.ptr = mlx_new_image(data->mlx, W_WIDTH, W_HEIGHT);
-    if (!data->img.ptr || !data->s_img.ptr)
+	if (!data->img.ptr || !data->s_img.ptr)
 		exit(ft_perror("Failed to create render image", 1));
-    data->img.addr = mlx_get_data_addr(data->img.ptr, &data->img.bpp, 
+	data->img.addr = mlx_get_data_addr(data->img.ptr, &data->img.bpp, 
 		&data->img.line_len, &data->img.endian);
 	data->s_img.addr = mlx_get_data_addr(data->s_img.ptr, &data->s_img.bpp, 
 		&data->s_img.line_len, &data->s_img.endian);
+}
+
+int ft_init_data(t_data *data, int fd)
+{
+	data->fd = fd;
 	data->curr = NULL;
-	data->err = NULL;;
+	data->err = NULL;
 	return(ft_init_cam(data));
 }
