d80241b198020383eeb543ec5e7607eab49bdfd4
atambo <alex.tambo.15432@gmail.com>
2025-05-07 12:04:46 +0100

    Trimeed ft_init_ray

--- src/ray.c
+++ src/ray.c
@@ -6,7 +6,7 @@
 /*   By: atambo <atambo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:22:34 by atambo            #+#    #+#             */
-/*   Updated: 2025/05/06 20:24:00 by atambo           ###   ########.fr       */
+/*   Updated: 2025/05/07 12:04:46 by atambo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ void	ft_init_ray(t_data *data,int x, int y, t_ray *ray)
 	ray->view_width = 2.0 * ray->tan_half_fov;
 	ray->asp_ratio = (float)IM_WIDTH / IM_HEIGHT;
 	ray->view_height = ray->view_width / ray->asp_ratio;
-	ray->u = (2.0 * (x + 0.5) / IM_WIDTH - 1.0) * ray->tan_half_fov;
-	ray->v = (1.0 - 2.0 * (y + 0.5) / IM_HEIGHT) * ray->view_height / 2.0;
 }
 void	ft_calc_ray(t_data *data,int x, int y, t_ray *ray)
 {
