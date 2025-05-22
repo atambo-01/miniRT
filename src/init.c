b717ab2b3ec22679d3b7663986735c7d623fee1b
atambo <alex.tambo.15432@gmail.com>
2025-05-22 11:12:26 +0100

    Finished with obj logic, nothing major left, going to start on light

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: atambo <atambo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 00:13:05 by atambo            #+#    #+#             */
-/*   Updated: 2025/05/21 19:44:10 by atambo           ###   ########.fr       */
+/*   Updated: 2025/05/22 11:01:15 by atambo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,18 @@ int	ft_init_data(t_data *data, int fd)
 	data->fd = fd;
 	data->mlx = mlx_init();
 	if (!data->mlx)
-		exit(1);
+		exit(ft_perror("Failed to init mlw connection\n", 1));
 	data->win = mlx_new_window(data->mlx, W_WIDTH, W_HEIGHT, "miniRT");
 	if (!data->win)
 	{
 		mlx_destroy_display(data->mlx);
 		free(data->mlx);
-		exit(1);
+		exit(ft_perror("Failed to init mlw window\n", 1));
 	}
 	data->img.ptr = mlx_new_image(data->mlx, IM_WIDTH, IM_HEIGHT);
 	data->s_img.ptr = mlx_new_image(data->mlx, W_WIDTH, W_HEIGHT);
 	if (!data->img.ptr || !data->s_img.ptr)
-		exit(ft_perror("Failed to create render image", 1));
+		exit(ft_perror("Failed to create mlx image\n", 1));
 	data->img.addr = mlx_get_data_addr(data->img.ptr, &data->img.bpp,
 			&data->img.line_len, &data->img.endian);
 	data->s_img.addr = mlx_get_data_addr(data->s_img.ptr, &data->s_img.bpp,
--- src/ray.c
+++ src/ray.c
@@ -6,7 +6,7 @@
 /*   By: atambo <atambo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:22:34 by atambo            #+#    #+#             */
-/*   Updated: 2025/05/07 12:04:46 by atambo           ###   ########.fr       */
+/*   Updated: 2025/05/22 11:09:44 by atambo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_init_ray(t_data *data,int x, int y, t_ray *ray)
 	ray->asp_ratio = (float)IM_WIDTH / IM_HEIGHT;
 	ray->view_height = ray->view_width / ray->asp_ratio;
 }
-void	ft_calc_ray(t_data *data,int x, int y, t_ray *ray)
+void	ft_calc_ray(t_data *data, int x, int y, t_ray *ray)
 {
 	ray->u = (2.0 * (x + 0.5) / IM_WIDTH - 1.0) * ray->tan_half_fov;
 	ray->v = (1.0 - 2.0 * (y + 0.5) / IM_HEIGHT) * ray->view_height / 2.0;
