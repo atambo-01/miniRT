7dbebab1e1f7f10e944ab38dd27ea87b0a7819f2
atambo <alex.tambo.15432@gmail.com>
2025-05-29 13:01:37 +0100

    Added alight still testing though

--- src/atambo/render_scene.c
+++ src/atambo/render_scene.c
@@ -6,7 +6,7 @@
 /*   By: atambo <atambo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 16:38:03 by atambo            #+#    #+#             */
-/*   Updated: 2025/05/29 11:20:55 by atambo           ###   ########.fr       */
+/*   Updated: 2025/05/29 12:50:19 by atambo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,32 +50,65 @@ int	ft_calc_hit(t_ray ray, t_obj *obj, t_hit *hit)
 
 }
 
-int	ft_ray_color_aux(int color, int shift, double intensity)
+int	ft_ray_color_aux(int color, double intensity)
 {
 	int	r;
+	int shift;
 
-	r = (color >> shift) & 0xFF;
-	r = (int)(r * intensity);
-	if (r > 255)
-		r = 255;
-	if (r < 0)
-		r = 0;
+		r = (color >> shift) & 0xFF;
+		r = (int)(r * intensity);
+		if (r > 255)
+			r = 255;
+		if (r < 0)
+			r = 0;
 	return (r);
 }
 
-int ft_add_alight(int color_1, t_alight lum)
+// Compute the contribution of a light source (ambient or direct) to an object's color
+void ft_light_effect(int color, int light, double ratio, int *rgb)
 {
-    int rgb[3];
+    // Extract object color components
+    int obj_rgb[3];
+    obj_rgb[0] = (color >> 16) & 0xFF; // Red
+    obj_rgb[1] = (color >> 8) & 0xFF;  // Green
+    obj_rgb[2] = color & 0xFF;         // Blue
 
-    // Extract base color components
-    rgb[0] = (color_1 >> 16) & 0xFF; // Red
-    rgb[1] = (color_1 >> 8) & 0xFF;  // Green
-    rgb[2] = color_1 & 0xFF;         // Blue
+    // Extract light color components
+    int light_rgb[3];
+    light_rgb[0] = (light >> 16) & 0xFF; // Red
+    light_rgb[1] = (light >> 8) & 0xFF;  // Green
+    light_rgb[2] = light & 0xFF;         // Blue
 
-    // Add scaled ambient light components
-    rgb[0] += ft_ray_color_aux(lum.color, 16, lum.ratio); // Red + ambient red
-    rgb[1] += ft_ray_color_aux(lum.color, 8, lum.ratio);  // Green + ambient green
-    rgb[2] += ft_ray_color_aux(lum.color, 0, lum.ratio);  // Blue + ambient blue
+    // Scale light by ratio and multiply by object color
+    rgb[0] = (int)(obj_rgb[0] * (light_rgb[0] / 255.0) * ratio);
+    rgb[1] = (int)(obj_rgb[1] * (light_rgb[1] / 255.0) * ratio);
+    rgb[2] = (int)(obj_rgb[2] * (light_rgb[2] / 255.0) * ratio);
+}
+
+void ft_ray_color(t_hit *hit, t_data *data, double x, double y)
+{
+    int rgb[3] = {0, 0, 0}; // Accumulated RGB values
+    int temp_rgb[3];        // Temporary RGB for each light contribution
+
+    if (hit->d >= 0)
+    {
+        int color = hit->color; // Object color
+        // Direct light contribution
+        int light = data->light.color;
+        double intensity = data->light.ratio * 1.0 / (1.0 + KAPPA * hit->d * hit->d);
+        ft_light_effect(color, light, intensity, temp_rgb);
+        rgb[0] += temp_rgb[0];
+        rgb[1] += temp_rgb[1];
+        rgb[2] += temp_rgb[2];
+    }
+
+    // Ambient light contribution
+    int light = data->alight.color;
+    double intensity = data->alight.ratio;
+    ft_light_effect(hit->color, light, intensity, temp_rgb);
+    rgb[0] += temp_rgb[0];
+    rgb[1] += temp_rgb[1];
+    rgb[2] += temp_rgb[2];
 
     // Clamp to [0, 255]
     for (int i = 0; i < 3; i++)
@@ -87,32 +120,8 @@ int ft_add_alight(int color_1, t_alight lum)
     }
 
     // Combine into final color
-    return (rgb[0] << 16) | (rgb[1] << 8) | rgb[2];
-}
-
-
-void ft_ray_color(t_hit *hit, t_data *data, double x, double y)
-{
-	int		color;
-	double	intensity;
-	int		rgb[3];
-	int		final_color;
-
-	if (hit->d >= 0)
-	{
-		color = hit->color;
-
-		intensity = data->light.ratio * 1.0 / (1.0
-				+ KAPPA * hit->d * hit->d);
-		rgb[0] = ft_ray_color_aux(color, 16, intensity);
-		rgb[1] = ft_ray_color_aux(color, 8, intensity);
-		rgb[2] = ft_ray_color_aux(color, 0, intensity);
-		final_color = (rgb[0]  << 16) | (rgb[1] << 8) | rgb[2];
-		final_color = ft_add_alight(final_color, data->alight);
-		ft_pixel_put_img(&data->img, x, y, final_color);
-	}
-	else
-		ft_pixel_put_img(&data->img, x, y, 0x000000);
+    int final_color = (rgb[0] << 16) | (rgb[1] << 8) | rgb[2];
+    ft_pixel_put_img(&data->img, x, y, final_color);
 }
 
 void	ft_render_scene(t_data *data)
