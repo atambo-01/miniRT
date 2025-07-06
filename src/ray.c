492aeac81463aacd2fd87442c3365788384eb7cc
atambo <alex.tambo.15432@gmail.om>
2025-07-06 12:09:21 +0100

    learned new logic for ray direction uses angles instead still trying to really get it

--- src/ray.c
+++ src/ray.c
@@ -6,7 +6,7 @@
 /*   By: atambo <atambo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:22:34 by atambo            #+#    #+#             */
-/*   Updated: 2025/05/06 20:24:00 by atambo           ###   ########.fr       */
+/*   Updated: 2025/07/06 12:06:56 by atambo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,34 @@ void	ft_init_ray(t_data *data,int x, int y, t_ray *ray)
 	ray->view_width = 2.0 * ray->tan_half_fov;
 	ray->asp_ratio = (float)IM_WIDTH / IM_HEIGHT;
 	ray->view_height = ray->view_width / ray->asp_ratio;
-	ray->u = (2.0 * (x + 0.5) / IM_WIDTH - 1.0) * ray->tan_half_fov;
-	ray->v = (1.0 - 2.0 * (y + 0.5) / IM_HEIGHT) * ray->view_height / 2.0;
 }
-void	ft_calc_ray(t_data *data,int x, int y, t_ray *ray)
-{
-	ray->u = (2.0 * (x + 0.5) / IM_WIDTH - 1.0) * ray->tan_half_fov;
-	ray->v = (1.0 - 2.0 * (y + 0.5) / IM_HEIGHT) * ray->view_height / 2.0;
-	ray->dir = (t_vec3){ray->u, ray->v, 1.0};
-	ft_normalize(&(ray->dir));
+
+void ft_calc_ray(t_data *data, int x, int y, t_ray *ray) {
+    float width = (float)IM_WIDTH;
+    float height = (float)IM_HEIGHT;
+    
+    // Convert FOV to radians
+    float fov_rad = data->cam.fov * M_PI / 180.0f; // Horizontal FOV
+    float vertical_fov_rad = fov_rad / ray->asp_ratio; // Vertical FOV
+    
+    // Angle for x-z plane (horizontal)
+    float angle_x = ((width - 1.0f) - 2.0f * x) / (width - 1.0f) * (fov_rad / 2.0f);
+    
+    // Angle for y-z plane (vertical)
+    float angle_y = ((height - 1.0f) - 2.0f * y) / (height - 1.0f) * (vertical_fov_rad / 2.0f);
+    
+    // Rotate in z-x plane, then z-y plane
+
+    // temp.x = 0 * cos(angle_x) + 1 * sin(angle_x) = sin(angle_x)
+    // temp.y = 0 = 0
+    // temp.z = 0 * (-sin(angle_x)) + 1 * cos(angle_x) = cos(angle_x)
+    // ray->dir.x = -sinf(angle_x);
+    // ray->dir.y = cosf(angle_x) * sinf(angle_y);
+    // ray->dir.z = cosf(angle_x) * cosf(angle_y);
+
+    ray->dir.x = -sin(angle_x);
+    ray->dir.y = cos(angle_x) * sin(angle_y);
+    ray->dir.z = cos(angle_x) * cos(angle_y);
+    
+    ft_normalize(&ray->dir);
 }
