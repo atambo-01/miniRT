4c25952a301a8a349e0afc161dc9dad5b03649e3
atambo <alex.tambo.15432@gmail.om>
2025-07-08 16:19:24 +0100

    added logic for ray direction

--- src/ray.c
+++ src/ray.c
@@ -6,7 +6,7 @@
 /*   By: atambo <atambo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:22:34 by atambo            #+#    #+#             */
-/*   Updated: 2025/07/06 12:06:56 by atambo           ###   ########.fr       */
+/*   Updated: 2025/07/08 16:18:09 by atambo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,28 +25,34 @@ void ft_calc_ray(t_data *data, int x, int y, t_ray *ray) {
     float width = (float)IM_WIDTH;
     float height = (float)IM_HEIGHT;
     
+	ray->dir = data->cam.dir;
     // Convert FOV to radians
-    float fov_rad = data->cam.fov * M_PI / 180.0f; // Horizontal FOV
-    float vertical_fov_rad = fov_rad / ray->asp_ratio; // Vertical FOV
+    float h_fov = data->cam.fov * M_PI / 180.0f; // Horizontal FOV
+    float v_fov = h_fov / ray->asp_ratio; // Vertical FOV
     
     // Angle for x-z plane (horizontal)
-    float angle_x = ((width - 1.0f) - 2.0f * x) / (width - 1.0f) * (fov_rad / 2.0f);
+    float angle_x = ((width - 1.0f) - 2.0f * x) / (width - 1.0f) * (h_fov / 2.0f);
     
     // Angle for y-z plane (vertical)
-    float angle_y = ((height - 1.0f) - 2.0f * y) / (height - 1.0f) * (vertical_fov_rad / 2.0f);
-    
-    // Rotate in z-x plane, then z-y plane
+    float angle_y = ((height - 1.0f) - 2.0f * y) / (height - 1.0f) * (v_fov / 2.0f);
+  
+    // The rotation matrix for rotating around the y-axis by θ radians is:
+    // |cos(θ)	0		sin(θ)	|
+    // |0		1		0		|
+    // |-sin(θ)	0		cos(θ)	|
 
-    // temp.x = 0 * cos(angle_x) + 1 * sin(angle_x) = sin(angle_x)
-    // temp.y = 0 = 0
-    // temp.z = 0 * (-sin(angle_x)) + 1 * cos(angle_x) = cos(angle_x)
-    // ray->dir.x = -sinf(angle_x);
-    // ray->dir.y = cosf(angle_x) * sinf(angle_y);
-    // ray->dir.z = cosf(angle_x) * cosf(angle_y);
+    ray->dir.x = ray->dir.x * cosf(angle_x) + ray->dir.z * sinf(angle_x);
+    ray->dir.y = ray->dir.y;
+    ray->dir.z = -(ray->dir.x) * sinf(angle_x) + ray->dir.z * cosf(angle_x);
 
-    ray->dir.x = -sin(angle_x);
-    ray->dir.y = cos(angle_x) * sin(angle_y);
-    ray->dir.z = cos(angle_x) * cos(angle_y);
+    // The rotation matrix for rotating around the x-axis by θ radians is:
+    // |1		0		0		|
+    // |0		cos(θ)	-sin(θ)	|
+    // |0		sin(θ)	cos(θ)	|
+
+    ray->dir.x = ray->dir.x;
+    ray->dir.y = ray->dir.y * cosf(angle_y) - ray->dir.z * sinf(angle_y);
+    ray->dir.z = (ray->dir.y) * sinf(angle_y) + ray->dir.z * cosf(angle_y);
     
     ft_normalize(&ray->dir);
 }
